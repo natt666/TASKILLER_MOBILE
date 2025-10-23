9a7a47637279a98bd9e84c5e1a4c353656204428
ashy404 <nobregaguerram@gmail.com>
2025-10-23 20:21:31 +0200

    he hecho avances en la pagina principal y casi he finaliazado el login (he dicho q lo he casi acabado 50 veces ya, lo se pero si :))

--- app/src/main/java/com/example/taskiller/LoginActivity.kt
+++ app/src/main/java/com/example/taskiller/LoginActivity.kt
@@ -17,8 +17,8 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
-        // Esto ajusta los márgenes para la barra del sistema :)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // Esto ajusta los márgenes para la barra del sistema :)s
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
