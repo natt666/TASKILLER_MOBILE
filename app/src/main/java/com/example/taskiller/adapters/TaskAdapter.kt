82bd193d6e81b5579f43e819d51b73496ade2e44
sgomezl <sgomezl2425@politecnics.barcelona>
2025-11-13 20:20:56 +0100

    progreso recycler tareas

--- app/src/main/java/com/example/taskiller/adapters/TaskAdapter.kt
+++ app/src/main/java/com/example/taskiller/adapters/TaskAdapter.kt
@@ -3,18 +3,19 @@ package com.example.taskiller.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskiller.R
 import com.example.taskiller.models.TarjetaTarea
 
-
 class TaskAdapter(private val lista: List<TarjetaTarea>) :
     RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val titulo: TextView = view.findViewById(R.id.titulo)
+        val titulo: TextView = view.findViewById(R.id.taskNameTextView)
+        val estado: TextView = view.findViewById(R.id.taskStatusTextView)
+        val fechaIni: TextView = view.findViewById(R.id.startDateTextView)
+        val fechaFin: TextView = view.findViewById(R.id.endDateTextView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -26,6 +27,9 @@ class TaskAdapter(private val lista: List<TarjetaTarea>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = lista[position]
         holder.titulo.text = item.titulo
+        holder.estado.text = item.estado
+        holder.fechaIni.text = item.fechaInicio as CharSequence?
+        holder.fechaFin.text = item.fechaFin as CharSequence?
     }
 
     override fun getItemCount(): Int = lista.size
