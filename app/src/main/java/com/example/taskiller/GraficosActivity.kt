7f81597fcb44dbd3a3c6a2b11874c86bd5467c38
Natalia Pérez Ruiz <nataaaliaia@gmail.com>
2025-12-09 20:25:52 +0100

    update piechart

--- app/src/main/java/com/example/taskiller/GraficosActivity.kt
+++ app/src/main/java/com/example/taskiller/GraficosActivity.kt
@@ -2,6 +2,9 @@ package com.example.taskiller
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.NumberPicker
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
@@ -16,6 +19,7 @@ import com.github.mikephil.charting.animation.Easing
 class GraficosActivity : AppCompatActivity() {
 
     private lateinit var pieChart: PieChart
+    private lateinit var graficosbtnvolver: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +27,14 @@ class GraficosActivity : AppCompatActivity() {
         setContentView(R.layout.activity_graficos1)
 
         pieChart = findViewById(R.id.pieChart)
+        graficosbtnvolver = findViewById<ImageButton>(R.id.graficosbtnvolver)
+        val lblnombreproyecto = findViewById<TextView>(R.id.lblGraficosNombreProyecto)
 
         val datos = getDatos()!!
         val proyecto = datos.listaProyectos.firstOrNull()
         val todasTareas = datos.listaTareas.filter { it.IdProyecto == proyecto?.Id }
 
+        lblnombreproyecto.text = proyecto?.Titulo
         // Contamos tareas por estado
         val estadoCounts = Proyecto.Estados.values().associateWith { estado ->
             todasTareas.count { it.Estado.name == estado.name }
@@ -38,9 +45,7 @@ class GraficosActivity : AppCompatActivity() {
         val tfMedium = ResourcesCompat.getFont(this, R.font.montserrat_medium)
 
         // Creamos entradas del PieChart, solo con >0
-        val entries = estadoCounts
-            .filter { it.value > 0 }
-            .map { (estado, count) ->
+        val entries = estadoCounts.filter { it.value > 0 }.map { (estado, count) ->
                 PieEntry(count.toFloat(), estado.name.replace("_", " "))
             }
 
@@ -65,12 +70,13 @@ class GraficosActivity : AppCompatActivity() {
         dataSet.selectionShift = 10f
         dataSet.valueTextSize = 14f
         dataSet.valueTextColor = Color.DKGRAY
-        dataSet.valueTypeface = tfBold
+        dataSet.valueTypeface = tfMedium
         dataSet.valueFormatter = PercentFormatter(pieChart)
         dataSet.yValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
         dataSet.xValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
-        dataSet.valueLinePart1Length = 0.4f
-        dataSet.valueLinePart2Length = 0.4f
+        dataSet.valueLinePart1OffsetPercentage = 55f
+        dataSet.valueLinePart1Length = 0.3f
+        dataSet.valueLinePart2Length = 0.2f
         dataSet.valueLineColor = Color.DKGRAY
 
         // PieChart general
@@ -82,7 +88,9 @@ class GraficosActivity : AppCompatActivity() {
         pieChart.setUsePercentValues(true)
         pieChart.setEntryLabelColor(Color.BLACK)
         pieChart.setEntryLabelTextSize(12f)
-        pieChart.setEntryLabelTypeface(tfMedium)
+        pieChart.setEntryLabelTypeface(tfBold)
+        pieChart.setExtraOffsets(30f, 10f, 30f, 10f)
+
 
         // Texto central
         pieChart.centerText = proyecto?.Titulo
@@ -101,11 +109,17 @@ class GraficosActivity : AppCompatActivity() {
         legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
         legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
         legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        legend.typeface = tfMedium
         legend.setDrawInside(false)
 
         // Asignar datos
         val data = PieData(dataSet)
         pieChart.data = data
         pieChart.invalidate()
+
+        graficosbtnvolver.setOnClickListener {
+            val itent
+        }
     }
+
 }
